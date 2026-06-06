57bf670cea802b0dd94001f32ad06eff9d7035dc
TurtleGods <strollist@gmail.com>
2026-06-06 10:48:45 +0800

    部屬API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GROUP_BUY_CONFIG = {
   // Deploy the Apps Script in google-apps-script.js as a Web App,
   // then paste the /exec URL here.
-  apiUrl: ""
+  apiUrl: "https://script.google.com/macros/s/1HnHw8fEDG2G7nB_kke50IBqCsQ8iPwjmIzHzPUu2ZST387uVDzTHJO4/execH"
 };
