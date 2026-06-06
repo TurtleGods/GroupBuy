65fbb8240ba4c017eb8fd5e24e49782f6d8ad4d4
TurtleGods <strollist@gmail.com>
2026-06-06 11:44:55 +0800

    改本script

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.GROUP_BUY_CONFIG = {
   // Deploy the Apps Script in google-apps-script.js as a Web App,
   // then paste the /exec URL here.
-  apiUrl: "https://script.google.com/macros/s/AKfycbzJApypba-B5sn9adonsRx-MsMuYJZhxAh1r4c1SKWbUR8JnstC3XKzvcWkOv7uRUbXdw/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbz9lekU54o_u_Gbl7EGBz1s4d0V9C06LhDeu7DffyeYJIR3zh5AH3fhNbc-Per2jk_uLA/exec"
 };
